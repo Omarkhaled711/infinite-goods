0c728cb6c3fd1b694764720ce8fd701e9aa53e6c
clairima <mertahrima@gmail.com>
2024-03-14 06:51:30 +0100

    send email activation

--- users/views.py
+++ users/views.py
@@ -3,7 +3,14 @@ from .forms import RegisterForm
 from .models import User
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import url_safe_base64_encode, url_safe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 
 def register(request):
     if request.method == 'POST':
@@ -18,6 +25,19 @@ def register(request):
             user = User.objects.create_user(first_name=first_name, last_name=last_name, email=email, user_name=user_name, password=password)
             user.phone_number = phone_number
             user.save()
+
+            # activation
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk))
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
             messages.success(request, 'Registration successful.')
             return redirect('register')
     else:
@@ -42,8 +62,15 @@ def login(request):
             messages.error(request, 'Invalid login cerdentials')
             return redirect('login')
     return render(request, 'login.html')
+
+
+
 @login_required(login_url='login')
 def logout(request):
     auth.logout(request)
     messages.success(request, 'You are logged out')
     return redirect('login')
+
+
+def activate(request, uidb64, token)
+    return HttpResponse()
