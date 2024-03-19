5a028435b669eaaa548333522f1dd2b5195fa19b
Omar Khaled <o.khaled6789@gmail.com>
2024-03-19 07:41:09 +0200

    integrated with paypal

--- infiniteGoods/static/js/script.js
+++ infiniteGoods/static/js/script.js
@@ -3,3 +3,29 @@ $(document).ready(function() {
         $('#message').fadeOut('slow');
     }, 5000);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Retrieve order total from data attribute
+    var grandTotal = parseFloat(document.getElementById('data-container').dataset.orderTotal);
+
+    // Configure PayPal Smart Buttons
+    paypal.Buttons({
+        createOrder: function(data, actions) {
+            return actions.order.create({
+                purchase_units: [{
+                    amount: {
+                        value: grandTotal
+                    }
+                }]
+            });
+        },
+        onApprove: function(data, actions) {
+            return actions.order.capture().then(function(details) {
+                console.log(details);
+                alert('Transaction completed by ' + details.payer.name);
+                // Call your server to save transaction details
+            });
+        }
+    }).render('#paypal-button-container');
+});
+
