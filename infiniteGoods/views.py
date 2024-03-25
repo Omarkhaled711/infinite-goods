582d0bcb2ce7d859c7bfc0e1acd1874779934335
clairima <mertahrima@gmail.com>
2024-03-25 14:00:40 +0100

    display rating stars on home

--- infiniteGoods/views.py
+++ infiniteGoods/views.py
@@ -2,16 +2,22 @@
 views model for main application
 '''
 from django.shortcuts import render
-from shop.models import Product
+from shop.models import Product, ReviewRating
 
 
 def home(req):
     '''
     The home page (root) of our website
     '''
-    products = Product.objects.all().filter(is_available=True)
+    products = Product.objects.all().filter(is_available=True).order_by('created_date')
+
+    for product in products:
+
+        reviews = ReviewRating.objects.filter(
+            product_id=product.id, status=True)
 
     context = {
         'products': products,
+        'reviews': reviews,
     }
     return render(req, 'index.html', context)
